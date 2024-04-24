8d6391d99cf966134fefdc3505fbd2f238a7ec11
kaval <kaval.itg@gmail.com>
2024-04-24 05:27:01 +0200

    Lagt till intents & knapp till sida

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.screens;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.loginbtn);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Inent(MainActivity.this, login)
+                Intent intent = new Intent(MainActivity.this, login.class);
+                intent.putExtra("name", "Test");
+                startActivity(intent);
             }
         });
     }
